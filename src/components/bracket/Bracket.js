362ca64a187f0bb9445b76e25d9d3d8b641b4fa7
Connor Bailey <bailey.connor.p@gmail.com>
2019-03-10 16:32:47 -0500

    preparebracketlist randomizes and cleans items from list

--- src/components/bracket/Bracket.js
+++ src/components/bracket/Bracket.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 
+import PrepareBracketList from "../../modules/bracket/PrepareBracketList"
+
 class Bracket extends Component {
     render() {
         const list = this.props.globalLists.find(list =>
             list.id === parseInt(this.props.match.params.listId)) || { id: 404, name: "No List Found " }
         const items = this.props.globalListItems.filter(item => item.listId === list.id)
-
-        console.log(list)
-        console.log(items)
+        // console.log(list)
+        // console.log(items)
+        const preparedArray = PrepareBracketList(items)
+        // console.log(preparedArray)
+        
 
 
         return (
             <React.Fragment>
                 <section className="">
-
                 </section>
             </React.Fragment>
         )
