2ac7cfbdfadba501ee7815171c295e631a4552ff
Connor Bailey <bailey.connor.p@gmail.com>
2019-03-10 20:04:48 -0500

    i think determine bracket attributes is coming up with correct dimensions based off of number of list items

--- src/modules/bracket/DetermineBracketAttributes.js
+++ src/modules/bracket/DetermineBracketAttributes.js
@@ -7,12 +7,34 @@ function DetermineBracketAttributes(array) {
         bracketInfo.columns = 3
     } else {
         bracketInfo.rows = (Math.pow(2, bracketInfo.rounds) - 1)
-        bracketInfo.columns = (bracketInfo.rounds*2) + 1
+        bracketInfo.columns = (bracketInfo.rounds * 2) + 1
     }
-    
     bracketInfo.columnInfo = []
-    for (var i = 0; i < bracketInfo.columns; i++) {
-        bracketInfo.columnInfo.push({})
+    let builderIndex = Math.ceil((bracketInfo.columns) / 2)
+    for (var i = builderIndex; i > 0; i--) {
+        let columnObj = {}
+        if (i === builderIndex) {
+            columnObj.top = Math.ceil(bracketInfo.rows / 2)
+            columnObj.inOut = 0
+            columnObj.bottom = columnObj.top - 1
+            columnObj.numItemsIn = 1
+            bracketInfo.columnInfo.push(columnObj)
+        } else if (i === builderIndex - 1) {
+            columnObj.top = Math.ceil(bracketInfo.rows / 2)
+            columnObj.inOut = 0
+            columnObj.bottom = columnObj.top - 1
+            columnObj.numItemsIn = 1
+            bracketInfo.columnInfo.unshift(columnObj)
+            bracketInfo.columnInfo.push(columnObj)
+        } else {
+            columnObj.top = Math.pow(2, i - 1)
+            columnObj.inOut = Math.pow(2, i)
+            columnObj.bottom = columnObj.top - 1
+            columnObj.numItemsIn = Math.pow(2, bracketInfo.rounds - i)
+            bracketInfo.columnInfo.unshift(columnObj)
+            bracketInfo.columnInfo.push(columnObj)
+        }
+
     }
     console.log(bracketInfo)
 }
