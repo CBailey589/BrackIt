7960124857f448782ae27d72cb6e2ccae30e0349
Connor Bailey <bailey.connor.p@gmail.com>
2019-03-12 15:23:22 -0500

    buttons are appearing in bracket, need functionality

--- src/components/bracket/BracketSquare.js
+++ src/components/bracket/BracketSquare.js
@@ -13,6 +13,21 @@ class BracketSqaure extends Component {
             <React.Fragment>
                 <div className={`${classList}`}>
                     {squareInfo.itemText}
+                    {
+                        (classList.includes("Button"))
+                            ? <div>
+                                <button key={`${col}-${row}-Above`}>
+                                    U
+                                </button>
+                                <button key={`${col}-${row}-Below`}>
+                                    D
+                                </button>
+                                <button key={`${col}-${row}-Rand`}>
+                                    R
+                                </button>
+                            </div>
+                            : null
+                    }
                 </div>
             </React.Fragment>
         )
