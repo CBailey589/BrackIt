e9193abe412407e71034fc15960afe4034f6b191
Connor Bailey <bailey.connor.p@gmail.com>
2019-03-13 13:09:47 -0500

    championship buttons are working correctly. ready for testing

--- src/modules/bracket/MakeBracketSquareInfo.js
+++ src/modules/bracket/MakeBracketSquareInfo.js
@@ -51,19 +51,22 @@ function MakeBracketSquareInfo(col, row, bracketObj) {
     }
 
     //Check to see if button should be in square
-    // if (firstLastCol === false && row > top && row < (bracketObj.rows - bottom + 1) && Math.floor((row - top) / (inOut + 0.1)) % 2 === 0) {
-    //     squareInfo.test = true
-    // }
     if (firstLastCol === false && row % inOut === 0 && (row / inOut) % 2 !== 0) {
         squareInfo.button = true
     } else if (firstLastCol === true && row % inOut === 0 && (row / inOut) % 2 !== 0 && ((row - top) / inOut) <= numItemsInCol) {
         squareInfo.button = true
     }
 
+    // Check to see if championship buttons should be in square
+    if (col === regionBreak + 1 && row === Math.ceil(bracketObj.rows / 2) + 1) {
+        squareInfo.champButton = true
+    }
 
 
 
-        let classList = []
+
+
+    let classList = []
     if (squareInfo.holdsItem === true) {
         classList.push("HoldsItem")
     }
@@ -72,6 +75,9 @@ function MakeBracketSquareInfo(col, row, bracketObj) {
     }
     if (squareInfo.button === true) {
         classList.push("ButtonSquare")
+    }
+    if (squareInfo.champButton === true) {
+        classList.push("ChampButtons")
     }
 
     squareInfo.classList = classList.join(" ")
