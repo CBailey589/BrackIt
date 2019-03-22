ce6c39afe796331eb7e3317e35e5c32743f79cfd
Connor Bailey <bailey.connor.p@gmail.com>
2019-03-22 09:25:26 -0500

    buttons and font sizing is initially ok, needs tweaking

--- src/components/bracket/BracketSquare.js
+++ src/components/bracket/BracketSquare.js
@@ -25,66 +25,45 @@ class BracketSqaure extends Component {
                     style={{
                         height: `${vh / numRows * squareScale}px`,
                         width: `${vw / numCols * squareScale}px`,
-                        fontSize: `${18 / rounds + (squareScale * 4.5) + (round * 2)}px`
+                        // ************************NEEDS TWEAKING***************************
+                        // fontSize: `${18 / rounds + (squareScale * 4.5) + (round * 2)}px`
+                        fontSize: `${3 + (squareScale ** 2) + (round * 2)}px`
                     }}
                 >
-                    {squareInfo.itemText}
+                    <div className="SquareText">
+                        {squareInfo.itemText}
+                    </div>
                     {
                         (classList.includes("ButtonSquare"))
-                            ? <div>
-                                <button key={`${round}-${row}-${col}-Above`}
-                                    id={`${round}-${row}-${col}`}
-                                    onClick={(evt) => {
-                                        this.props.advanceItemToNextRound(1, evt)
-                                    }}
-                                >
-                                    U
-                                </button>
-                                <button key={`${round}-${row}-${col}-Below`}
+                            ? <div className="ButtonDiv">
+                                <div key={`${round}-${row}-${col}-Below`}
                                     id={`${round}-${row}-${col}`}
                                     onClick={(evt) => {
                                         this.props.advanceItemToNextRound(3, evt)
                                     }}
-                                >
-                                    ?
-                                </button>
-                                <button key={`${round}-${row}-${col}-Randj`}
-                                    id={`${round}-${row}-${col}`}
-                                    onClick={(evt) => {
-                                        this.props.advanceItemToNextRound(2, evt)
+                                    style={{
+                                        height: `${(1 * (squareScale + 1)) + (round * 3)}px`,
+                                        width: `${(1 * (squareScale + 1)) + (round * 3)}px`,
+                                        // height: `20px`,
+                                        // width: `20px`,
+                                        backgroundColor: `red`
                                     }}
                                 >
-                                    D
-                                </button>
+
+                                </div>
                             </div>
                             : null
                     }
                     {
                         (classList.includes("ChampButtons"))
                             ? <div>
-                                <button key={`${row}-${col}-Left`}
-                                    id={`${round}-${row}-${col}`}
-                                    onClick={(evt) => {
-                                        this.props.pickChamp(1, evt)
-                                    }}
-                                >
-                                    L
-                                </button>
                                 <button key={`${row}-${col}-Right`}
                                     id={`${round}-${row}-${col}`}
                                     onClick={(evt) => {
                                         this.props.pickChamp(3, evt)
                                     }}
                                 >
-                                    ?
-                                </button>
-                                <button key={`${row}-${col}-Rand`}
-                                    id={`${round}-${row}-${col}`}
-                                    onClick={(evt) => {
-                                        this.props.pickChamp(2, evt)
-                                    }}
-                                >
-                                    R
+
                                 </button>
                             </div>
                             : null
@@ -96,3 +75,84 @@ class BracketSqaure extends Component {
 }
 
 export default BracketSqaure
+
+                // {/* <React.Fragment>
+                // <div
+                //     className={`${classList}`}
+                //     id={`${col}${row}--${squareInfo.itemKey}`}
+                //     style={{
+                //         height: `${vh / numRows * squareScale}px`,
+                //         width: `${vw / numCols * squareScale}px`,
+                //         // ************************NEEDS TWEAKING***************************
+                //         // fontSize: `${18 / rounds + (squareScale * 4.5) + (round * 2)}px`
+                //         fontSize: `${0 + (squareScale * 4.5) + (round * 2)}px`
+                //     }}
+                // >
+                //     {squareInfo.itemText}
+                //     {
+                //         (classList.includes("ButtonSquare"))
+                //             ? <div className="ButtonDiv">
+                //                 <button key={`${round}-${row}-${col}-Above`}
+                //                     id={`${round}-${row}-${col}`}
+                //                     onClick={(evt) => {
+                //                         this.props.advanceItemToNextRound(1, evt)
+                //                     }}
+                //                     style={{
+                //                         height: `${(1 * (squareScale + 1)) + (round * 3)}px`,
+                //                         width: `${(1 * (squareScale + 1)) + (round * 3)}px`,
+                //                         backgroundColor: `red`
+                //                     }}
+                //                 >
+                //                     {/* U */}
+                //                 </button>
+                //                 <button key={`${round}-${row}-${col}-Below`}
+                //                     id={`${round}-${row}-${col}`}
+                //                     onClick={(evt) => {
+                //                         this.props.advanceItemToNextRound(3, evt)
+                //                     }}
+                //                 >
+                //                     {/* ? */}
+                //                 </button>
+                //                 <button key={`${round}-${row}-${col}-Randj`}
+                //                     id={`${round}-${row}-${col}`}
+                //                     onClick={(evt) => {
+                //                         this.props.advanceItemToNextRound(2, evt)
+                //                     }}
+                //                 >
+                //                     {/* D */}
+                //                 </button>
+                //             </div>
+                //             : null
+                //     }
+                //     {
+                //         (classList.includes("ChampButtons"))
+                //             ? <div>
+                //                 <button key={`${row}-${col}-Left`}
+                //                     id={`${round}-${row}-${col}`}
+                //                     onClick={(evt) => {
+                //                         this.props.pickChamp(1, evt)
+                //                     }}
+                //                 >
+                //                     L
+                //                 </button>
+                //                 <button key={`${row}-${col}-Right`}
+                //                     id={`${round}-${row}-${col}`}
+                //                     onClick={(evt) => {
+                //                         this.props.pickChamp(3, evt)
+                //                     }}
+                //                 >
+                //                     ?
+                //                 </button>
+                //                 <button key={`${row}-${col}-Rand`}
+                //                     id={`${round}-${row}-${col}`}
+                //                     onClick={(evt) => {
+                //                         this.props.pickChamp(2, evt)
+                //                     }}
+                //                 >
+                //                     R
+                //                 </button>
+                //             </div>
+                //             : null
+                //     }
+                // </div>
+                // </React.Fragment >
